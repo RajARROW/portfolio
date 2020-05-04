607132abcb3809f3c47209953d0f4da55a6edcdd
Raj <raj.150410116036@gmail.com>
2020-05-04 21:31:40 +0530

    first commit

---
+++ src/components/header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="container mx-auto">
+    <nav class="flex item-center justify-end">
+      <a id="work-ref" href="#work" class="p-4 text-secondary uppercase font-bold text-lg md:text-xl">Work</a>
+      <a href="#about" class="p-4 text-secondary uppercase font-bold text-lg md:text-xl">About</a>
+      <a class="p-4 text-secondary uppercase font-bold text-lg md:text-xl">Contect</a>
+    </nav>
+  </header>
+</template>
---
+++ src/components/home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <app-banner></app-banner>
+        <app-project></app-project>
+        <app-about></app-about>
+    </div>
+</template>
